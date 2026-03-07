ed0f36814332f83aff9ffecd3830f8168cd2a2c5
aakash3141 <aakashrp786@gmail.com>
2026-03-06 18:35:18 -0600

    Add files via upload
    The code that *used* to work...

--- encoder.h
+++ encoder.h
@@ -1,12 +1,48 @@
-// as said in the file "encoder.py" and group chat microPython may not be fast enough for a PID controller since
-// we need the feedback as fast as possible. I wrote the code in Arduino/C++
-#ifndef ENCODER_H
-#define ENCODER_H
+#include <Arduino.h>
 
-// Initialize encoder pins and interrupts
-void encoderInit();
+// Encoder pins (OMRON E6B2)
+#define ENC_A 0  // Encoder channel A
+#define ENC_B 1  // Encoder channel B
 
-// Get current encoder count
-long encoderGetCount();
+#define PPR 1024.0 // pulses per rotation
+#define PI 3.14159
+#define VELOCITY_DELAY 10
 
-#endif
+// Use volatile because this value is changed inside an interrupt
+volatile long encoderCount = 0;
+
+// Interrupt service routine: triggered on channel A change
+void encoderISR() {
+  // Read channel B to determine rotation direction
+  if (digitalRead(ENC_B) == digitalRead(ENC_A)) {
+    encoderCount++;  // Forward rotation
+  } else {
+    encoderCount--;  // Backward rotation
+  }
+}
+
+// Initialize encoder pins and attach interrupts
+void encoderInit() {
+  pinMode(ENC_A, INPUT_PULLUP);  // A channel input with pull-up
+  pinMode(ENC_B, INPUT_PULLUP);  // B channel input with pull-up
+
+  // Attach interrupt on rising and falling edges for maximum accuracy
+  attachInterrupt(digitalPinToInterrupt(ENC_A), encoderISR, CHANGE);
+}
+  
+// Return current encoder count
+long encoderGetCount() {
+  return encoderCount;
+}
+
+float encoderGetAngle() {
+  return (2 * PI * encoderCount / (2 * PPR)); // Divide by 2 * PPR since we are incrementing on both rising and falling edges
+}
+
+float encoderGetAngularVelocity() {
+  float theta1 = encoderGetAngle();
+  delay(VELOCITY_DELAY);
+  float theta2 = encoderGetAngle();
+
+  return (1000 * (theta2 - theta1) / VELOCITY_DELAY);
+}
